f31c8a65456166911a0f1b2106786bc5052f679f
Ricardo Macedo <ricardomrs@gmail.com>
2021-10-18 18:24:21 -0300

    Refactor index.html and main-star.js, remove variants and comments

--- javascript/components/main-star.js
+++ javascript/components/main-star.js
@@ -1,7 +1,3 @@
-//abrir window
-//abrir browser
-//achar a estrela
-
 const star = window.document.querySelectorAll('.main-star');
 
 for(i = 0; i < star.length; i++){
@@ -14,22 +10,4 @@ for(i = 0; i < star.length; i++){
         }
     })    
 }
-
-//const estrela = window.document.querySelectorAll('.main-star.-teste');
-
-//for(i=0; i < estrela.length; i++){
-  //  estrela[i].style = "background: url('https://api.iconify.design/codicon/star-full.svg?color=%23c4c4c4&height=14') no-repeat center center / contain;";
-    //console.log(estrela[i].style);
-    //estrela[i].addEventListener('click', a => {
-      //  console.log(a);
-       // console.log(a.target.attributes.style.textContent);
-        //if(a.target.attributes.style.textContent == "background: rgba(0, 0, 0, 0) url(\"https://api.iconify.design/codicon/star-full.svg?color=%23c4c4c4&height=14\") no-repeat scroll center center / contain;"){
-          //  a.target.style = "background: url('https://api.iconify.design/codicon/star-full.svg?color=%23F03D3F&height=14') no-repeat center center / contain;";
-
-//        }else{
-  //          a.target.style = "background: url('https://api.iconify.design/codicon/star-full.svg?color=%23c4c4c4&height=14') no-repeat center center / contain;";
-
-//        }
-  //  })
-//};
 
