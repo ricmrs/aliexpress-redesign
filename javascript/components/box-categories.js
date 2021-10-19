033a688f48796309c7c6113b129c8cf80e138d98
Ricardo Macedo <ricardomrs@gmail.com>
2021-10-19 13:49:12 -0300

    Add box-items.css in box-categories.js

--- javascript/components/box-categories.js
+++ javascript/components/box-categories.js
@@ -1,10 +1,25 @@
 const btn = document.querySelector('.main-button.-second');
-const box = document.querySelector('.box-categories')
+const box = document.querySelector('.box-categories');
 
 btn.addEventListener('pointerover', function() {
     box.style = "visibility: visible;"
-})
+});
 
 btn.addEventListener('pointerout', function() {
     box.style = "visibility: hidden;"
-})
+    items.style = "visibility: hidden;"
+});
+
+const items = document.querySelector('.box-items');
+
+box.addEventListener('pointerover', function() {
+    items.style = "visibility: visible;"
+});
+
+items.addEventListener('pointerover', function() {
+    items.style = "visibility: visible;"
+});
+
+items.addEventListener('pointerout', function() {
+    items.style = "visibility: hidden;"
+});
