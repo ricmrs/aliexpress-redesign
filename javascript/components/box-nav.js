e440acad6a2c045d0bb9e4ab08540640c1845594
Ricardo Macedo <ricardomrs@gmail.com>
2021-10-18 23:46:10 -0300

    Add z-index in box-nav.css and remove console.log in box-nav.js

--- javascript/components/box-nav.js
+++ javascript/components/box-nav.js
@@ -1,9 +1,6 @@
 const btn = document.querySelector('.main-button.-second');
 const box = document.querySelector('.box-nav')
 
-console.log(btn);
-console.log(box);
-
 btn.addEventListener('pointerover', function() {
     box.style = "visibility: visible;"
 })
