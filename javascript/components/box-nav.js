7b4a2d9e13ee9415655324bb934d429224f100db
Ricardo Macedo <ricardomrs@gmail.com>
2021-10-18 22:29:29 -0300

    Create function in box-nav.js

--- javascript/components/box-nav.js
+++ javascript/components/box-nav.js
@@ -4,3 +4,10 @@ const box = document.querySelector('.box-nav')
 console.log(btn);
 console.log(box);
 
+btn.addEventListener('pointerover', function() {
+    box.style = "visibility: visible;"
+})
+
+btn.addEventListener('pointerout', function() {
+    box.style = "visibility: hidden;"
+})
