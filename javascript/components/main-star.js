7dbe6d48f7d5c30761636de15c7d930028d6f5c6
Ricardo Macedo <ricardomrs@gmail.com>
2021-12-16 08:45:05 -0300

    Add hover rating feature

--- javascript/components/main-star.js
+++ javascript/components/main-star.js
@@ -1,9 +1,10 @@
-function starRate(event){
+const $star = document.querySelectorAll('.main-star');
+
+function hoverRate(event){
     const $origin = event.target
     const $siblings = $origin.parentNode.children
-    const white = "url('https://api.iconify.design/codicon/star-full.svg?color=%23c4c4c4&height=14') center center / contain no-repeat"
     const orange = "url('https://api.iconify.design/codicon/star-full.svg?color=%23F03D3F&height=14') center center / contain no-repeat"
-    rate($siblings, 4, white)
+    cleanRate(event)
     for(i = 0; i <= 4; i++){
         if($origin === $siblings[i]){
             rate($siblings, i, orange);
@@ -12,8 +13,30 @@ function starRate(event){
     }
 }
 
-function rate(stars, total, color) {
+function cleanRate(event){
+    const $origin = event.target
+    const $siblings = $origin.parentNode.children
+    const white = "url('https://api.iconify.design/codicon/star-full.svg?color=%23c4c4c4&height=14') center center / contain no-repeat"
+    rate($siblings, 4, white)
+}
+
+function rate(stars, total, color){
     for(i = 0; i <= total; i++){
         stars[i].style.background = color;
     }
+}
+
+function clickRate(event){
+    const $origin = event.target
+    const $siblings = $origin.parentNode.children
+    for(i = 0; i <= 4; i++){
+        $siblings[i].removeEventListener('mouseenter', hoverRate)
+        $siblings[i].removeEventListener('mouseout', cleanRate)
+        $siblings[i].addEventListener('click', hoverRate)
+    }
+}
+
+for(i = 0; i < $star.length; i++){
+    $star[i].addEventListener('mouseenter', hoverRate)
+    $star[i].addEventListener('mouseout', cleanRate)
 }
